9aad124e9b732519d3ebde8ad15fbc859d249baf
shaphill <113211180+shaphill@users.noreply.github.com>
2023-10-04 11:24:06 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    dcnm = {
+      # The CiscoDevNet/dcnm provider supports both NDFC and DCNM
+      source = "CiscoDevNet/dcnm"
+    }
+  }
+}
+
+provider "dcnm" {
+  username = "admin"
+  password = "ins3965!"
+  url      = "https://10.0.223.207"
+  insecure = true
+  platform = "dcnm"
+}
+
+resource "dcnm_inventory" "cml" {
+  fabric_name     = "cml"
+  username        = "cisco"
+  password        = "cisco"
+  max_hops        = 2
+  preserve_config = "false"
+  auth_protocol   = 0
+  config_timeout  = 10
+
+  switch_config {
+    ip   = "10.0.223.185"
+    role = "border_gateway_spine"
+  }
+  switch_config {
+    ip   = "10.0.223.186"
+    role = "spine"
+  }
+  switch_config {
+    ip   = "10.0.223.187"
+    role = "leaf"
+  }
+  switch_config {
+    ip   = "10.0.223.188"
+    role = "leaf"
+  }
+  switch_config {
+    ip   = "10.0.223.189"
+    role = "leaf"
+  }
+  switch_config {
+    ip   = "10.0.223.190"
+    role = "leaf"
+  }
+}
